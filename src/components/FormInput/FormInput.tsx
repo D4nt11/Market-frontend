37162d6c1db16da43b8472e0fe592d43426c6065
D4nt11 <Profesor07@yandex.ru>
2024-12-19 00:19:10 +0300

    make validation of form

--- src/components/FormInput/FormInput.tsx
+++ src/components/FormInput/FormInput.tsx
@@ -1,10 +1,10 @@
 import styles from './FormInput.module.css'
 
-const FormInput = (props: any) => {
+const FormInput = ({name, type, register, registerName, rules}: any) => {
   return (
     <div className={styles.FormInput}>
-        <label htmlFor="formInput">{props.name}</label>
-        <input type={props.type} id='formInput'/>
+        <label htmlFor="formInput">{name}</label>
+        <input {...register(registerName, rules)} type={type} id='formInput'/>
     </div>
   )
 }
