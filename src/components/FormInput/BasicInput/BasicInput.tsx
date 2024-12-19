83c9626424323d5a81d5d882e39553b7dbf7091b
D4nt11 <Profesor07@yandex.ru>
2024-12-19 17:01:15 +0300

    make validation form

---
+++ src/components/FormInput/BasicInput/BasicInput.tsx
@@ -0,0 +1,12 @@
+import styles from './BasicInput.module.css'
+
+const BasicInput = ({name, type, register, registerName, rules}: any) => {
+  return (
+    <div className={styles.BasicInput}>
+    <label htmlFor="basicInput">{name}</label>
+    <input {...register(registerName, rules)} type={type} id='basicInput'/>
+</div>
+  )
+}
+
+export default BasicInput
--- src/pages/Auth/SignUpClient/SignUpClient.tsx
+++ src/pages/Auth/SignUpClient/SignUpClient.tsx
@@ -1,19 +1,20 @@
 import { useForm } from "react-hook-form";
-import FormInput from "../../../components/FormInput/FormInput";
 import styles from "./SignUpClient.module.css";
 import * as yup from 'yup'
 import { yupResolver } from "@hookform/resolvers/yup";
+import BasicInput from "../../../components/FormInput/BasicInput/BasicInput";
+import PhoneInput from "../../../components/FormInput/PhoneInput/PhoneInput";
 
 
 const SignUpClient = () => {
 
-
-  const schema = yup.object({
-    firstName: yup.string().required(),
-    surName: yup.string().required(),
-    phone: yup.string().required(),
-    email: yup.string().email().required(),
-    password: yup.string().required(),
+  const schema = yup.object().shape({
+    firstName: yup.string().trim().required('Введите имя'),
+    surName: yup.string().trim().required('Введите фамилию'),
+    // phone: yup.string().required('Введите номер телефона').matches(/^\+7 \(\d{3}\) \d{3}-\d{2}-\d{2}$/,
+    //   'Введите корректный номер телефона в формате +7 (xxx) xxx-xx-xx'),
+    email: yup.string().required('Введите почту').matches(/^\S+@\S+\.\S+$/, 'Неверный формат почты'),
+    password: yup.string().required('Введите пароль'),
   }).required()
 
 
@@ -22,23 +23,31 @@ const SignUpClient = () => {
     console.log(data);
   }
   
+  
   return (
     <form onSubmit={handleSubmit(registration)} className={styles.main}>
       <h1>Регистрация</h1>
       <h5>
         Уже есть аккаунт? <span>Войти</span>
       </h5>
-      <FormInput name="Имя" type="text" register={register} registerName="firstName" rules={{required: true}}/>
+      <BasicInput name="Имя" type="text" register={register} registerName="firstName" rules={{required: true}}/>
       <p>{errors.firstName?.message}</p>
-      <FormInput name="Фамилия" type="text" register={register} registerName="surName" rules={{required: true}}/>
-      <FormInput name="Телефон" type="text" register={register} registerName="phone" rules={{required: true}}/>
-      <FormInput name="Почта" type="email" register={register} registerName="email" rules={{required: true}}/>
-      <FormInput name="Пароль" type="password" register={register} registerName="password" rules={{required: true}}/>
+      <BasicInput name="Фамилия" type="text" register={register} registerName="surName" rules={{required: true}}/>
+      <p>{errors.surName?.message}</p>
+      <PhoneInput name="Телефон" register={register} registerName="phone" rules={{required: true}}/>
+      {/* <p>{errors.phone?.message}</p> */}
+      <BasicInput name="Почта" type="email" register={register} registerName="email" rules={{required: true}}/>
+      <p>{errors.email?.message}</p>
+      <BasicInput name="Пароль" type="password" register={register} registerName="password" rules={{required: true}}/>
+      <p>{errors.password?.message}</p>
+      <div className={styles.checkboxContainer}>
+      <input type="checkbox" />
       <h5 className={styles.h5}>
         При создании аккаунта Вы соглашаетесь с нашими{" "}<br />
         <span className={styles.span}>Условиями</span> и{" "}
         <span className={styles.span}>Политикой конфиденциальности.</span>
       </h5>
+      </div>
       <input className={styles.button} type="submit" value="Зарегистрироваться" />
     </form>
   );
